3f9c96dadabd1a5bf1e28892797fe5a66b10fd07
yanniiie <150505258+yanniiie@users.noreply.github.com>
2024-05-17 11:53:16 +0600

    Update File Compression.cpp

--- File Compression.cpp
+++ File Compression.cpp
@@ -1,1 +1,24 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+     ofstream newFile("Compressed.txt");
+     ifstream fin("text.txt");
+     int words;
+     string text,s;
+     vector<char> ch;
+     string str;
+     string q="my name is Feruz";
+     while(getline(fin,str)){
+          int n = str.length();
+          for (int i = 0; i < n; i++) {
+               int count = 1;
+               while (i < n - 1 && str[i] == str[i + 1]) {
+                    count++;
+                    i++;
+               }
+        
+               newFile<<str[i]<<count;
+          }
+          newFile<<endl;
+     }
+}
